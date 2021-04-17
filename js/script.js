4f1c6e32b8478f17dc0dc17f6a9557df95fd10e1
Fai <chepngetichfaith46@gmail.com>
2021-04-17 17:10:15 +0300

    Add indentation

---
+++ js/script.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+    $(".design").click(function(){
+       $("p").toggle();
+    //    $(".describe-design").show();
+    $(".welcome").click(function(){
+        $("p").toggle();
+      });
+
+    })
+})
