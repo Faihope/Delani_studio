ed2789351f6412280611fabd98265e1abd5cd9c1
Fai <chepngetichfaith46@gmail.com>
2021-04-17 17:11:35 +0300

    Add indentation

--- js/script.js
+++ js/script.js
@@ -1,10 +1,5 @@
 $(document).ready(function(){
-    $(".design").click(function(){
-       $("p").toggle();
-    //    $(".describe-design").show();
-    $(".welcome").click(function(){
-        $("p").toggle();
-      });
+    
 
-    })
+    
 })
