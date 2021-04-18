1b1bfbe427a1f4a2fb331bc1cdc763b807c24479
Fai <chepngetichfaith46@gmail.com>
2021-04-18 12:08:58 +0300

    Add the hide and show effects on the develop and product management images

--- js/script.js
+++ js/script.js
@@ -1,7 +1,34 @@
 $(document).ready(function(){
     
     $("#design").click(function(){
-$("#describe-design").toggle();
+$("#describe-design").show();
+$("#design").hide();
+
     });
+    $("#describe-design").click(function(){
+        $("#design").show();
+        $("#describe-design").hide();
+
+    });
+    $("#develop").click(function(){
+        $("#describe-develop").show();
+        $("#develop").hide();
+        
+            });
+            $("#describe-develop").click(function(){
+                $("#develop").show();
+                $("#describe-develop").hide();
+        
+            });
+            $("#product").click(function(){
+                $("#describe-product").show();
+                $("#product").hide();
+                
+                    });
+                    $("#describe-product").click(function(){
+                        $("#product").show();
+                        $("#describe-product").hide();
+                
+                    });
     
 })
