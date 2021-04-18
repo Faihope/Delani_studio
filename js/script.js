17f08ce40844e1a22e6b2a58f4965ab981542722
Fai <chepngetichfaith46@gmail.com>
2021-04-18 17:09:58 +0300

    Add hover effects on the rest of the portfolio images

--- js/script.js
+++ js/script.js
@@ -65,5 +65,41 @@ $(document).ready(function () {
         $("#four").hide();
 
     });
+    // hover effect on image five
+    $(".five").mouseenter(function () {
+        $("#five").show();
+
+    });
+    $(".five").mouseleave(function () {
+        $("#five").hide();
+
+    });
+    // hover effect on image six
+    $(".six").mouseenter(function () {
+        $("#six").show();
+
+    });
+    $(".six").mouseleave(function () {
+        $("#six").hide();
+
+    });
+    // hover effect on image seven
+    $(".seven").mouseenter(function () {
+        $("#seven").show();
+
+    });
+    $(".seven").mouseleave(function () {
+        $("#seven").hide();
+
+    });
+    // hover effect on image eight
+    $(".eight").mouseenter(function () {
+        $("#eight").show();
+
+    });
+    $(".eight").mouseleave(function () {
+        $("#eight").hide();
+
+    });
 
 })
