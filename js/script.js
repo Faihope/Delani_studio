cbdfba2faa4a1b63b55772e5abe428e919735aa9
Fai <chepngetichfaith46@gmail.com>
2021-04-18 16:50:23 +0300

    Add hover effect on the second image

--- js/script.js
+++ js/script.js
@@ -1,42 +1,51 @@
-$(document).ready(function(){
-    $("#design").click(function(){
-$("#describe-design").show();
-$("#design").hide();
+$(document).ready(function () {
+    $("#design").click(function () {
+        $("#describe-design").show();
+        $("#design").hide();
 
     });
-    $("#describe-design").click(function(){
+    $("#describe-design").click(function () {
         $("#design").show();
         $("#describe-design").hide();
 
     });
-    $("#develop").click(function(){
+    $("#develop").click(function () {
         $("#describe-develop").show();
         $("#develop").hide();
-        
-            });
-            $("#describe-develop").click(function(){
-                $("#develop").show();
-                $("#describe-develop").hide();
-        
-            });
-            $("#product").click(function(){
-                $("#describe-product").show();
-                $("#product").hide();
-                
-                    });
-                    $("#describe-product").click(function(){
-                        $("#product").show();
-                        $("#describe-product").hide();
-                
-                    });
-                    $(".one").mouseenter(function(){
-                        $("#one").show();
 
-                    });
-                    $(".one").mouseleave(function(){
-                        $("#one").hide();
+    });
+    $("#describe-develop").click(function () {
+        $("#develop").show();
+        $("#describe-develop").hide();
 
-                    });
+    });
+    $("#product").click(function () {
+        $("#describe-product").show();
+        $("#product").hide();
 
-    
+    });
+    $("#describe-product").click(function () {
+        $("#product").show();
+        $("#describe-product").hide();
+
+    });
+    // hover effects on portfolio image one
+    $(".one").mouseenter(function () {
+        $("#one").show();
+
+    });
+    $(".one").mouseleave(function () {
+        $("#one").hide();
+
+    });
+    // hover effect on image two
+    $(".two").mouseenter(function () {
+        $("#two").show();
+
+    });
+    $(".two").mouseleave(function () {
+        $("#two").hide();
+
+    });
+
 })
